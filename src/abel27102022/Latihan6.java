a03434389d7fedc5f1469dedd21018fae818b041
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2022-10-28 11:08:50 +0700

    Create Latihan6.java

---
+++ src/abel27102022/Latihan6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abel27102022;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan6{
+ public static void main(String args[]){
+     int a=0;
+     while (a<100){
+         
+	   System.out.println("ABEL");
+	   a++;
+	 }
+    }
+}
