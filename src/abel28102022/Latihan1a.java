9fe3a1bf0cbbdea7e04556e6e0d0fff5ca15933d
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2022-10-28 11:11:45 +0700

    Create Latihan1a.java

---
+++ src/abel28102022/Latihan1a.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abel28102022;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan1a {
+    public static void main(String[] args) {
+        String hari [] ={"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
+        int i=0;
+        while (i < hari.length){
+            System.out.println(hari[i]);
+            i++;
+        }
+    }
+    
+}
